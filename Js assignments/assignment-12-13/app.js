7671a8bbef10ed9f220430b394dca6b8c3e100f4
Dua Shaikh <99713184+minahilirfan26@users.noreply.github.com>
2023-05-16 13:19:40 +0500

    Add files via upload

---
+++ Js assignments/assignment-12-13/app.js
@@ -0,0 +1,103 @@
+/*===========QUESTION#01===========
+Write a program that takes a character (number or string)
+in a variable & checks whether the given input is a
+number, uppercase letter or lower case letter. (Hint: ASCII
+codes:- A=65, Z=90, a=97, z=122).
+**/
+
+
+/*===========QUESTION#02===========
+Write a JavaScript program that accept two integers and
+display the larger. Also show if the two integers are equal.
+**/
+
+//let num1 = +prompt("Enter the num here!");
+//let num2 = +prompt("Enter the num2 here!");
+//if (num1>num2) {
+//    document.write(num1+" is gerater");
+//} else if (num2>num1){
+//    document.write(num2+" is greater")
+//} else{
+//    document.write("Both nums r equals")
+//};
+
+
+/*===========QUESTION#03===========
+Write a program that takes input a number from user &
+state whether the number is positive, negative or zero.
+**/
+
+//let num = +prompt("Enter a num");
+//if (num<0) {
+//    document.write(num+" Negative");
+//} else if (num>0){
+//    document.write(num+" positive");
+//} else{
+//    document.write("Zero")
+//};
+
+
+/*===========QUESTION#05===========
+Write a program that
+a. Store correct password in a JS variable.
+b. Asks user to enter his/her password
+c. Validate the two passwords:
+i. Check if user has entered password. If not, then
+give message “ Please enter your password”
+ii. Check if both passwords are same. If they are
+same, show message “Correct! The password you
+entered matches the original password”. Show
+“Incorrect password” otherwise.
+**/
+
+//let password = prompt("enter ur pass");
+//let confirmPass = prompt("Confirm ur pass");
+//
+//if (password.length===0) {
+//    document.write("Please enter ur pass")
+//} else if (password===confirmPass){
+//    document.write("Correct! The password you entered matches the original password")
+//} else{
+//    document.write("Incorrect password")
+//};
+
+
+/*===========QUESTION#06===========
+This if/else statement does not work. Try to fix it:
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
+else
+greeting = "Good evening";
+}
+**/
+
+//ar greeting;
+//ar hour = 13;
+//f (hour < 18) {
+//reeting = "Good day";
+//ocument.write(greeting);
+//
+//lse{
+//reeting = "Good evening";
+//ocument.write(greeting)
+//;
+
+
+/*===========QUESTION#07===========
+Write a program that takes time as input from user in 24
+hours clock format like: 1900 = 7pm. Implement the
+following case using if, else & else if statements
+**/
+
+//let time = prompt("Enter the time in 24 hours clocks");
+//if (time >=0000 && time<1200) {
+//    document.write("Good Morning")
+//} else if(time >=1200 && time<1700){
+//    document.write("Good afternoon")
+//} else if(time >=1700 && time<2100){
+//    document.write("Good Evening")
+//} else if(time >=2100 && time<2359){
+//    document.write("Good Ni8")
+//}
